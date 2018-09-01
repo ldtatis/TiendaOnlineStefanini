54ef9ac2b717a0701f79ff6af9ad394c3837e3ff
Leidy Tatiana Alzate Hurtado <leidy-t96@hotmail.com>
2018-09-01 15:46:00 -0500

    Pagina principal
    Se eliminaron controladores innecesarios
    y se establecio como principal la pagina de categorias

--- TiendaOnline/Controllers/HomeController.cs
+++ TiendaOnline/Controllers/HomeController.cs
@@ -10,21 +10,8 @@ namespace TiendaOnline.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return RedirectToAction("Index", "Categorias", new { area = "Admin" });
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        
     }
 }
